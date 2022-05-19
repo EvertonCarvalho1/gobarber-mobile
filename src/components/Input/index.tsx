5e2909e38b726a842f62401b5da26f65e51dcaa9
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-05-19 19:50:37 -0300

    integrating unform

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -15,23 +15,35 @@ interface InputValueReference {
 }
 
 const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
-    const inputValueRef = useRef<InputValueReference>({ value: '' });;
-    const { registerField, defaultValue, fieldName, error } = useField(name);
+    const inputElementRef = useRef<any>(null); 
+
+    const { registerField, defaultValue = '', fieldName, error } = useField(name);
+    const inputValueRef = useRef<InputValueReference>({ value: defaultValue });;
 
     useEffect(() => {
-        registerField({
+        registerField<string>({
             name: fieldName,
             ref: inputValueRef.current,
-            path: 'value'
+            path: 'value',
+            setValue(ref: any, value){
+                inputValueRef.current.value = value;
+                inputElementRef.current.setNativeProps({text: value});
+            },
+            clearValue(){
+                inputValueRef.current.value = '';
+                inputElementRef.current.clear;
+            }
         });
-    }, []);
+    }, [fieldName, inputValueRef]);
 
     return (
         <Container >
             <Icon name={icon} size={20} color='#666360' />
             <TextInput
+                ref={inputElementRef}
                 keyboardAppearance="dark"
                 placeholderTextColor='#666360'
+                defaultValue={defaultValue}
                 onChangeText={(value) => {
                     inputValueRef.current.value = value;
                 }}
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import logoImg from '../../assets/logo.png';
 import Input from "../../components/Input";
@@ -10,6 +12,7 @@ import Button from "../../components/Button";
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 
 const SignUp: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
     const navigation = useNavigation();
     return (
 
@@ -30,16 +33,18 @@ const SignUp: React.FC = () => {
                         <View>
                             <Title>Crie sua conta</Title>
                         </View>
+                        <Form ref={formRef} onSubmit={(data) => { console.log(data) }}>
+                            <Input name="name" icon="user" placeholder="Nome" />
 
-                        <Input name="name" icon="user" placeholder="Nome" />
+                            <Input name="email" icon="mail" placeholder="E-mail" />
 
-                        <Input name="email" icon="mail" placeholder="E-mail" />
+                            <Input name="password" icon="lock" placeholder="Senha" />
 
-                        <Input name="password" icon="lock" placeholder="Senha" />
 
-                        <Button onPress={() => { console.log('boa') }}>
-                            Cadastrar
-                        </Button>
+                            <Button onPress={() => formRef.current?.submitForm()}>
+                                Cadastrar
+                            </Button>
+                        </Form>
 
                     </Container>
                 </ScrollView>

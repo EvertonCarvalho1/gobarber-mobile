34dbe33b8cec3369279c8475297fd9f049a793bb
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-05-24 19:57:23 -0300

    dashboard header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,16 +3,20 @@ import { View, Button } from 'react-native';
 
 import { useAuth } from '../../hooks/auth';
 
+import { Container, Header, HeaderTitle, UserName } from "./styles";
+
 const Dashboard: React.FC = () => {
     const { signOut } = useAuth();
 
     return (
-        <View>
-            <Button
-                title="Sair"
-                onPress={() => signOut()}
-            />
-        </View>
+        <Container>
+            <Header>
+                <HeaderTitle>
+                    Bem vindo, {'\n'}
+                    <UserName>Everton Carvalho</UserName>
+                </HeaderTitle>
+            </Header>
+        </Container>
     )
 }
 
